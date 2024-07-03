00470f49ae0113d87af1e919bd831a1b4756a3b1
Prachi5791 <patilmg2009@gmail.com>
2024-07-03 10:17:18 +0530

    chnages done

--- src/routes/(tools)/online-poll-tool/+page.svelte
+++ src/routes/(tools)/online-poll-tool/+page.svelte
@@ -1,6 +1,7 @@
 
 <script>
-    // JavaScript code here
+    
+
     function share() {
         alert('Sharing poll...');
     }
@@ -14,9 +15,25 @@
     function closeModal() {
         modalActive = false;
     }
+
+    const votes = [
+        { option: 'Option 1', count: 1 },
+        { option: 'Option 2', count: 2 },
+        { option: 'Option 3', count: 2 }
+    ];
+
+    // Calculate total votes
+    let totalVotes = votes.reduce((total, vote) => total + vote.count, 0);
+
+    // Calculate percentage for each option
+    const getPercentage = (count) => (totalVotes ? (count / totalVotes) * 100 : 0);
 </script>
 
 <style>
+
+svg path {
+        fill: #fff; 
+    }
     /* CSS styles here */
     .outer-container {
         display: flex;
@@ -76,16 +93,13 @@
         background: #fff;
         border-radius: 10px;
     }
-    .bar .line:before {
-        content: '';
-        position: absolute;
+        .bar .line > div {
         height: 100%;
-        top: 0;
-        left: 0;
         background: pink;
         border-radius: 10px;
-        width: 95%;
     }
+
+
     .button {
         display: flex;
         gap: 250px;
@@ -108,8 +122,16 @@
         font-weight: 500;
     }
     .btn2 {
-        justify-content: right;
+        display: flex;
+        justify-content: space-evenly;
     }
+
+    .btn2 svg{
+        color: #fff;
+        width: 20px;
+        height: 20px;
+    }
+
     .modal {
         z-index: 999999;
         position: fixed;
@@ -149,6 +171,11 @@
 	}
 
 	.live-btn {
+        display: flex;
+        flex-direction: column;
+        justify-self: center;
+        align-items: center;
+        gap: 2px;
         width: auto;
         text-align: center;
         border-radius: 6px;
@@ -158,6 +185,12 @@
         margin-top: 24px;
         font-size: 14px;
         font-weight: 500;
+    }
+
+    .share{
+        width: 40px;
+        height: 40px;
+        color: #fff;
     }
 
     @media(max-width: 1250px) {
@@ -195,59 +228,62 @@
 
         <div class="title">
             <h2>Title</h2>
-            <!-- <p>by a guest</p> -->
+         
         </div>
 
         <div class="results">
-            <ul class="bar">
-                <li class="options">
-                    <div class="info">
-                        <h1>Option 1</h1>
-                        <h1>90% (1 votes)</h1>
-                    </div>
-                    <div class="line"></div>
-                </li>
-
-                <li class="options">
-                    <div class="info">
-                        <h1>Option 2</h1>
-                        <h1>90% (1 votes)</h1>
-                    </div>
-                    <div class="line"></div>
-                </li>
-
-                <li class="options">
-                    <div class="info">
-                        <h1>Option 3</h1>
-                        <h1>90% (1 votes)</h1>
-                    </div>
-                    <div class="line"></div>
-                </li>
+        
+             <ul class="bar">
+                {#each votes as { option, count }}
+                    <li class="options">
+                        <div class="info">
+                            <h1>{option}</h1>
+                            <h1>{getPercentage(count).toFixed(1)}% ({count} votes)</h1>
+                        </div>
+                        <div class="line">
+                            <div style="width: {getPercentage(count)}%;"></div>
+                        </div>
+                    </li>
+                {/each}
             </ul>
         </div>
 
 		<div class="title">
-            <p>Total votes: </p>    
+           
+           <p>Total votes: {totalVotes}</p> 
         </div>
 
         <div class="button">
             <div class="btn">
-                <button class="live-result-btn">Live Results</button>
-                <button class="live-result-btn">Back to Poll</button>
+                <button class="btn2 live-result-btn">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm64 192c17.7 0 32 14.3 32 32v96c0 17.7-14.3 32-32 32s-32-14.3-32-32V256c0-17.7 14.3-32 32-32zm64-64c0-17.7 14.3-32 32-32s32 14.3 32 32V352c0 17.7-14.3 32-32 32s-32-14.3-32-32V160zM320 288c17.7 0 32 14.3 32 32v32c0 17.7-14.3 32-32 32s-32-14.3-32-32V320c0-17.7 14.3-32 32-32z"/></svg>
+                    Live Results</button>
+                <button class="btn2 live-result-btn">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/></svg>
+                    Back to Poll</button>
             </div>
-            <div class="btn2">
-                <button class="live-result-btn" on:click={modalfun}>Share</button>
+            <div class="btn2 live-result-btn">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"> <path d="M352 224c53 0 96-43 96-96s-43-96-96-96s-96 43-96 96c0 4 .2 8 .7 11.9l-94.1 47C145.4 170.2 121.9 160 96 160c-53 0-96 43-96 96s43 96 96 96c25.9 0 49.4-10.2 66.6-26.9l94.1 47c-.5 3.9-.7 7.8-.7 11.9c0 53 43 96 96 96s96-43 96-96s-43-96-96-96c-25.9 0-49.4 10.2-66.6 26.9l-94.1-47c.5-3.9 .7-7.8 .7-11.9s-.2-8-.7-11.9l94.1-47C302.6 213.8 326.1 224 352 224z"/></svg>
+                <!-- //remove<button class="" on:click={modalfun}>Share</button> -->
+
+                <button class="" on:click={modalfun}>Share</button>
             </div>
         </div>
 
-        <!-- Modal -->
-        <div class="modal" class:active={modalActive}>
+        <!-- Modal  -->
+        <div class="modal {modalActive ? 'active' : ''}" >
             <div class="modal-inner">
                 <h2>Share this poll</h2>
                 <div class="button-container">
-                    <button class="share-button live-btn" on:click={share}>Share on Facebook</button>
-                    <button class="share-button live-btn" on:click={share}>Share on Twitter</button>
-                    <button class="share-button live-btn" on:click={share}>Share via Email</button>
+                    <button class="share-button live-btn" on:click={share}>
+                        <svg class="share" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64h98.2V334.2H109.4V256h52.8V222.3c0-87.1 39.4-127.5 125-127.5c16.2 0 44.2 3.2 55.7 6.4V172c-6-.6-16.5-1-29.6-1c-42 0-58.2 15.9-58.2 57.2V256h83.6l-14.4 78.2H255V480H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64z"/></svg>
+                        Share on Facebook</button>
+                    <button class="share-button live-btn" on:click={share}>
+                        <svg class="share" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm297.1 84L257.3 234.6 379.4 396H283.8L209 298.1 123.3 396H75.8l111-126.9L69.7 116h98l67.7 89.5L313.6 116h47.5zM323.3 367.6L153.4 142.9H125.1L296.9 367.6h26.3z"/></svg>
+                        Share on Twitter</button>
+                    <button class="share-button live-btn" on:click={share}>
+                        <svg class="share" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"/></svg>
+                        Share via Email</button>
                 </div>
                 <button class="live-result-btn" on:click={closeModal}>Close</button>
             </div>
@@ -255,3 +291,5 @@
 
     </div>
 </div>
+
+
